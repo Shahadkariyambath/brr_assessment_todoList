a95342100803770ae88645e557a965c6485d840c
shahadkariyambath <shahadchikku@gmail.com>
2024-02-28 18:50:52 +0000

    implemented handleChange and submitList function to handle todo items and display the items.

--- src/component/todoApp.jsx
+++ src/component/todoApp.jsx
@@ -2,13 +2,44 @@ import React from "react";
 import "./todoApp.css";
 
 export default function todoApp() {
+  const [item, setItem] = React.useState("");
   const [list, setList] = React.useState([]);
 
+  const handleChange = (e) => {
+    // console.log(input);
+    setItem(e.target.value);
+  };
+
+  const submitList = (e) => {
+    e.preventDefault();
+    if (!item) return;
+    setList([...list, item]);
+    setItem("");
+  };
+
+  console.log(list);
   return (
     <div className="todo-container">
-      <form className="input-section">
+      <form className="input-section" onSubmit={(e) => submitList(e)}>
         <h1>Todo List</h1>
+
+        <input
+          type="text"
+          name="input"
+          value={item}
+          onChange={(e) => handleChange(e)}
+          placeholder="Enter items..."
+        />
       </form>
+      <ul>
+        {list &&
+          list.map((value, key) => (
+            <li key={key}>
+              <input type="checkbox" />
+              <div className="value">{value}</div>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
