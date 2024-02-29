77f2f12d11df68ca33dd04df2fa949d2f0b0670f
shahadkariyambath <shahadchikku@gmail.com>
2024-02-29 08:46:13 +0000

    Implemented checkbox and its logic

--- src/component/todoApp.jsx
+++ src/component/todoApp.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./todoApp.css";
 
 export default function todoApp() {
   const [item, setItem] = useState("");
-  const [list, setList] = useState([]);
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    const storedTodo = JSON.parse(localStorage.getItem("todoListStore"));
+    console.log("count", storedTodo);
+
+    if (storedTodo) {
+      setTodo(storedTodo);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoListStore", JSON.stringify(todo));
+  }, [todo]);
 
   const handleChange = (e) => {
     setItem(e.target.value);
@@ -12,19 +25,34 @@ export default function todoApp() {
   const submitList = (e) => {
     e.preventDefault();
     if (!item || !item.trim()) return;
-    setList([...list, item]);
+    setTodo([...todo, { list: item, checked: false }]);
     setItem("");
   };
 
   const editItem = (index) => {
     if (!item) {
-      setItem(list.slice(index, index + 1)[0]);
-      setList(list.filter((data, key) => key !== index));
+      setItem(todo[index].list);
+      setTodo(todo.filter((data, key) => key !== index));
+      console.log("editItem", todo);
     }
   };
 
   const deleteItem = (index) => {
-    setList(list.filter((data, key) => key !== index));
+    setTodo(todo.filter((data, key) => key !== index));
+    console.log("deleteItem", todo);
+  };
+
+  const toggleChecked = (index) => {
+    const updatedTodo = todo.map((value, key) => {
+      if (key === index) {
+        value.checked = !value.checked;
+      }
+      return value;
+    });
+
+    setTodo(updatedTodo);
+
+    console.log("toggleChecked", todo[index].checked);
   };
 
   return (
@@ -41,12 +69,16 @@ export default function todoApp() {
         />
       </form>
       <ul>
-        {list &&
-          list.map((value, index) => (
+        {todo &&
+          todo.map((value, index) => (
             <li key={index}>
               <div className="list-section">
-                <input type="checkbox" />
-                <div className="value">{value}</div>
+                <input
+                  type="checkbox"
+                  checked={value.checked}
+                  onChange={() => toggleChecked(index)}
+                />
+                <div className="value">{value.list}</div>
               </div>
               <div className="action-bar">
                 <i className="fas fa-edit" onClick={() => editItem(index)}></i>
